f69f3d842ac0a025ec5d8f3c43b1cc66739a2738
Sriram <sriram.shamrao74@gmail.com>
2024-08-09 16:48:54 +0530

    Update models.py

--- PartyAppAPI/models.py
+++ PartyAppAPI/models.py
@@ -10,12 +10,12 @@ class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True)
-    password = Column(String)
-    email = Column(String, unique=True)
-    role = Column(String)
+    username = Column(String(20), unique=True)
+    password = Column(String(150))
+    email = Column(String(50), unique=True)
+    role = Column(String(10))
     is_active = Column(Boolean)
-    mobile = Column(String)
+    mobile = Column(String(10))
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
 
 
@@ -23,21 +23,21 @@ class Location(Base):
     __tablename__ = 'location'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-    address = Column(String)
+    name = Column(String(50))
+    description = Column(String(100))
+    address = Column(String(100))
     pincode = Column(Integer, nullable=False)
-    city = Column(String)
-    state = Column(String)
+    city = Column(String(15))
+    state = Column(String(25))
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
 
 
 class Theater(Base):
-    __tablename__ = 'theaters'
+    __tablename__ = 'theater'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
+    name = Column(String(25))
+    description = Column(String(100))
     price = Column(Integer)
     no_of_peoples = Column(Integer)
     extra_cost_each_person = Column(Integer)
@@ -51,12 +51,12 @@ class PartyEvent(Base):
     __tablename__ = 'party_event'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-    spacial_name = Column(String)
-    spacial_other_name = Column(String)
+    name = Column(String(25))
+    description = Column(String(100))
+    spacial_name = Column(String(50))
+    spacial_other_name = Column(String(50))
     price = Column(Integer)
-    image_path = Column(String)
+    image_path = Column(String(100))
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
 
 
@@ -64,12 +64,12 @@ class AddOns(Base):
     __tablename__ = 'addons'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-    type = Column(String)
+    name = Column(String(25))
+    description = Column(String(100))
+    type = Column(String(25))
     quantity = Column(Integer)
     price = Column(Integer)
-    image_path = Column(String)
+    image_path = Column(String(100))
     is_available = Column(Boolean)
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
 
@@ -78,11 +78,11 @@ class Gallery(Base):
     __tablename__ = 'gallery'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String(25))
     location = Column(Integer, ForeignKey("location.id"))
     theater = Column(Integer, ForeignKey("theater.id"))
-    description = Column(String)
-    image_path = Column(String)
+    description = Column(String(100))
+    image_path = Column(String(100))
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
 
 
@@ -90,10 +90,10 @@ class Slots(Base):
     __tablename__ = 'slots'
 
     id = Column(Integer, primary_key=True, index=True)
-    slot_time_duration = Column(String)
+    slot_time_duration = Column(String(15))
     slot_date = Column(DateTime, default=datetime.utcnow)
     is_active = Column(Boolean)
-    theatre = Column(Integer, ForeignKey('theatre.id'))
+    theater = Column(Integer, ForeignKey('theater.id'))
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
 
 
@@ -102,14 +102,14 @@ class BookingEntry(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     price_total = Column(Integer)
-    theatre = Column(Integer, ForeignKey('theatre.id'))
+    theater = Column(Integer, ForeignKey('theater.id'))
     location = Column(Integer, ForeignKey('location.id'))
     party_event_selected = Column(Integer, ForeignKey('party_event.id'))
     date = Column(DateTime, default=datetime.utcnow)
     slot = Column(Integer, ForeignKey('slots.id'))
     no_of_peoples = Column(Integer)
-    addons_selected = Column(String)
-    booking_name = Column(String)
+    addons_selected = Column(String(50))
+    booking_name = Column(String(50))
     booking_mobile = Column(Integer)
-    booking_email = Column(String)
+    booking_email = Column(String(50))
     created_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
