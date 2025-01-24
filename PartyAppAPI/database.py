00ed83e038074347dca820ef20affaa116530b02
sriram kulkarni <sriram.shamrao74@gmail.com>
2025-01-24 13:27:14 +0530

    updated

--- PartyAppAPI/database.py
+++ PartyAppAPI/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/fastDB'
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/fastDB"
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://ebfuncity_ebfuncity:FastDB2024@74.50.122.78:3306/ebfuncity_fastDB'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://ebfuncity_ebfuncity:FastDB2024@66.85.161.154:3306/ebfuncity_fastDB'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=10, pool_timeout=5)
 # engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
